c462f2afa8a173936eb3a33b7e6f61307591f7ce
SherryChouu <sherrychou0710@gmail.com>
2022-04-29 16:11:37 +0800

    0429

--- src/com/fju/order/Order.java
+++ src/com/fju/order/Order.java
@@ -4,8 +4,13 @@ public class Order {
     int amount;
     Delivery delivery;
 
-    public Order(int amount, Delivery delivery) {
+    public Order(int amount, Delivery delivery, int shipFeed) {
         this.amount = amount;
         this.delivery = delivery;
+    }
+    
+    public int total(){
+        int total = amount + delivery.price;
+        return total();
     }
 }
